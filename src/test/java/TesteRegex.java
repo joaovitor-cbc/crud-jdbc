528d490bcf9908198a9457a05cdb265a85f3e481
joaovitor-cbc <joaovitor.novacruz@gmail.com>
2020-08-31 20:33:38 -0300

    Sugundo Commit

--- src/test/java/TesteRegex.java
+++ src/test/java/TesteRegex.java
@@ -11,7 +11,6 @@
 public class TesteRegex {
     public static void main(String[] args) {
         String regex= "^\\d{1,12}+(\\,)?+(\\d{0,2})$";
-        
         System.out.println("teste1".matches(regex));
         System.out.println("12".matches(regex));
         System.out.println("12,12".matches(regex));
